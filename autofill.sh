bc480426ebdd0526c9cfb1f71f2d033028e97c03
LeBigSky <nexagray@gmail.com>
2022-04-22 12:43:09 +0200

    Ajout affichage des fonctions

--- autofill.sh
+++ autofill.sh
@@ -9,3 +9,22 @@
 # Date: MARS 2022 - Version: Alpha v1                   #
 #                                                       #
 #########################################################
+echo " 
+ ________________________________________________________________
+|                      AUTOFILL-INDEX                            |
+|            Création et remplissage de l'index                  |
+|________________________________________________________________|"
+echo " 
+         --> Opérer les options suivantes de manière automatique:
+
+AU MENU DE CE SCRIPT:
+ ______________________________________________________________________________________________
+|	                                                                                       |
+|	-> création d'un fichier index.html (ou d'un autre nom)                                |
+|	-> ajout des infos & metas de bases nécéssaire au fichiers html ver: utf-8 french      |
+|	-> ajout des liens et des scripts de Bootstrap                                         |
+|	-> ajout de fontawesome ver:5                                                          |
+|	-> ajout de la structure de base: html/head/body                                       |
+|	-> ajout de structures avancées: header/nav/footer                                     |
+|______________________________________________________________________________________________|
+"
