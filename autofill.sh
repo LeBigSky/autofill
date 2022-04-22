4a63e50870b5eec3b4d87d5d92dd2c56e24c1315
LeBigSky <nexagray@gmail.com>
2022-04-22 11:53:24 +0200

    ajout de l'entête du scripte

---
+++ autofill.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#########################################################
+#                   AUTOFILL-INDEX                      #       
+#                                                       #
+# Description: Remplissage auto de l'index htlm         #
+#                                                       #
+# Auteur: Gary "El-Padre"                               #
+#                                                       #
+# Date: MARS 2022 - Version: Alpha v1                   #
+#                                                       #
+#########################################################
